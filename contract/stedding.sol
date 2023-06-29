9fd06cffe94af9b5118365b2f4c7e5b5a1bfa294
Andrew MacPherson <github@andrewmohawk.com>
2023-06-29 00:43:29 -0400

    Added voting

--- contract/stedding.sol
+++ contract/stedding.sol
@@ -4,16 +4,49 @@ pragma solidity ^0.8.7;
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 contract Stedding is ChainlinkClient, Ownable, ReentrancyGuard {
     using Chainlink for Chainlink.Request;
 
-    uint256 oraclePayment;
-    bytes32 jobId;
-    // This array will keep track of all entries for iteration
+    uint256 public oraclePayment;
+    bytes32 public jobId;
     string[] public entryKeys;
-    // Add this state variable
-bool public initialRootDomainSet = false;
+
+    address public LINK_TOKEN_ADDRESS;
+
+    enum ProposalType {
+        Add,
+        Remove
+    }
+    enum Vote {
+        None,
+        InFavor,
+        Against
+    }
+
+    struct Proposal {
+        ProposalType proposalType;
+        string rootDomain;
+        uint256 inFavor;
+        uint256 against;
+        mapping(string => Vote) votes; // map rootDomain to Vote
+        bool resolved;
+    }
+
+    uint256 public rootDomainCount;
+    uint256 public proposalCount;
+    mapping(uint256 => Proposal) public proposals;
+
+    // Events
+    event ProposalCreated(
+        uint256 proposalId,
+        ProposalType proposalType,
+        string rootDomain,
+        string creator
+    );
+    event Voted(uint256 proposalId, string voterRootDomain, Vote vote);
+    event ProposalResolved(uint256 proposalId, bool passed);
 
     struct Entry {
         string entry;
@@ -27,135 +60,168 @@ bool public initialRootDomainSet = false;
         address sender;
     }
 
-    address[] public rootDomainOwners;
-    mapping(address => bool) public isRootDomainOwner;
     mapping(string => Entry) public allowList;
     mapping(bytes32 => string) public requestIdToEntry;
     mapping(string => bool) public isValidRootDomain;
     mapping(bytes32 => DnsVerificationRequest) public dnsVerificationRequests;
 
-    event EntryAdded(string entry, string comment);
+    event EntryAdded(bytes32 indexed requestId, string entry, string comment);
     event EntryUpdated(string entry, string newComment);
     event EntryVerified(string entry);
     event EntryVerificationSucceeded(string entry);
     event EntryVerificationFailed(string entry, string reason);
+    event RootDomainAdded(string rootDomain);
     event RootDomainRemoved(string rootDomain);
 
+    modifier validRootDomain(string memory rootDomain) {
+        require(isValidRootDomain[rootDomain], "Invalid root domain");
+        _;
+    }
+
     constructor() Ownable() {
-        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        address _linkTokenAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+        LINK_TOKEN_ADDRESS = _linkTokenAddress;
+        setChainlinkToken(_linkTokenAddress);
         setChainlinkOracle(0xB9756312523826A566e222a34793E414A81c88E1);
         jobId = "791bd73c8a1349859f09b1cb87304f71";
         oraclePayment = 0.1 * 10 ** 18;
-        rootDomainOwners.push(msg.sender);
-        isRootDomainOwner[msg.sender] = true;
     }
 
-    // Function to set the initial root domain
-function setInitialRootDomain(string memory rootDomain) external onlyOwner {
-    require(!initialRootDomainSet, "Initial root domain already set");
-    isValidRootDomain[rootDomain] = true;
-    initialRootDomainSet = true;
+    function setOraclePayment(uint256 newPayment) external onlyOwner {
+        oraclePayment = newPayment;
+    }
+
+    function addRootDomain(string memory rootDomain) public onlyOwner {
+        require(bytes(rootDomain).length > 0, "Root domain cannot be empty");
+        isValidRootDomain[rootDomain] = true;
+        rootDomainCount++;
+        emit RootDomainAdded(rootDomain);
+    }
+
+    function removeRootDomain(string memory rootDomain) public onlyOwner {
+        require(bytes(rootDomain).length > 0, "Root domain cannot be empty");
+        isValidRootDomain[rootDomain] = false;
+        rootDomainCount--;
+        emit RootDomainRemoved(rootDomain);
+    }
+
+    function createProposal(ProposalType proposalType, string memory rootDomain)
+    external
+    validRootDomain(rootDomain)
+{
+    require(bytes(rootDomain).length > 0, "Root domain cannot be empty");
+
+    proposalCount++;
+
+    Proposal storage newProposal = proposals[proposalCount];
+    newProposal.proposalType = proposalType;
+    newProposal.rootDomain = rootDomain;
+    newProposal.inFavor = 0;
+    newProposal.against = 0;
+    newProposal.resolved = false;
+
+    emit ProposalCreated(proposalCount, proposalType, rootDomain, rootDomain);
 }
 
-// Function to add more root domains
-function addRootDomain(string memory rootDomain) external onlyOwner {
-    require(initialRootDomainSet, "Initial root domain must be set first");
-    isValidRootDomain[rootDomain] = true;
-}
 
-    function addressToString(address _addr) internal pure returns (string memory) {
-        bytes32 value = bytes32(uint256(uint160(_addr)));
-        bytes memory alphabet = "0123456789abcdef";
-
-        bytes memory str = new bytes(42);
-        str[0] = '0';
-        str[1] = 'x';
-        for (uint256 i = 0; i < 20; i++) {
-            str[2 + i * 2] = alphabet[uint8(value[i + 12] >> 4)];
-            str[3 + i * 2] = alphabet[uint8(value[i + 12] & 0x0f)];
-        }
-        return string(str);
-    }
-
-    function requestDnsVerification(string memory entry, string memory rootDomain, address sender) internal {
-        Chainlink.Request memory req = buildChainlinkRequest(jobId, address(this), this.dnsVerificationFulfillment.selector);
-        req.add("name", rootDomain);
-        req.add("record", addressToString(sender));
-        bytes32 requestId = sendChainlinkRequest(req, oraclePayment);
-
-        dnsVerificationRequests[requestId] = DnsVerificationRequest(entry, sender);
-    }
-
-    function dnsVerificationFulfillment(bytes32 _requestId, bool _isVerified) external nonReentrant recordChainlinkFulfillment(_requestId) {
-        DnsVerificationRequest memory dnsRequest = dnsVerificationRequests[_requestId];
-
-        if (_isVerified) {
-            allowList[dnsRequest.entry].isVerified = true;
-            emit EntryVerificationSucceeded(dnsRequest.entry);
-        } else {
-            emit EntryVerificationFailed(dnsRequest.entry, "DNS Verification Failed");
-        }
-
-        delete dnsVerificationRequests[_requestId];
-    }
-
-    function addEntry(
-        string memory entry,
-        string memory rootDomain,
-        string memory comment
-    ) public {
-        require(allowList[entry].isVerified == false, "Entry already exists");
-        require(isValidRootDomain[rootDomain], "Invalid root domain");
-        allowList[entry] = Entry(entry, rootDomain, comment, false);
-        entryKeys.push(entry);  // Store the key
-        requestDnsVerification(entry, rootDomain, msg.sender);
-        emit EntryAdded(entry, comment);
-    }
-
-    function updateEntry(
-        string memory entry,
-        string memory rootDomain,
-        string memory newComment
-    ) public {
-        require(allowList[entry].isVerified, "Entry does not exist");
-        require(isValidRootDomain[rootDomain], "Invalid root domain");
-
-        string memory existingRootDomain = allowList[entry].rootDomain;
+    function vote(uint256 proposalId, Vote voteChoice)
+        external
+        validRootDomain(allowList[entryKeys[0]].rootDomain)
+    {
+        require(proposalId > 0 && proposalId <= proposalCount, "Invalid proposal ID");
+        Proposal storage proposal = proposals[proposalId];
+        require(!proposal.resolved, "Proposal already resolved");
         require(
-            keccak256(abi.encodePacked(existingRootDomain)) ==
-                keccak256(abi.encodePacked(rootDomain)),
-            "Root domain mismatch"
+            proposal.votes[allowList[entryKeys[0]].rootDomain] == Vote.None,
+            "Already voted"
         );
 
-        allowList[entry].isVerified = false;
-        allowList[entry].comment = newComment;
-        requestDnsVerification(entry, rootDomain, msg.sender);
+        if (voteChoice == Vote.InFavor) {
+            proposal.inFavor++;
+        } else {
+            proposal.against++;
+        }
 
-        emit EntryUpdated(entry, newComment);
+        proposal.votes[allowList[entryKeys[0]].rootDomain] = voteChoice;
+        emit Voted(proposalId, allowList[entryKeys[0]].rootDomain, voteChoice);
+
+        // Check resolution
+        if (proposal.inFavor > rootDomainCount / 2) {
+            proposal.resolved = true;
+            emit ProposalResolved(proposalId, true);
+            if (proposal.proposalType == ProposalType.Add) {
+                addRootDomain(proposal.rootDomain);
+            } else {
+                removeRootDomain(proposal.rootDomain);
+            }
+        } else if (proposal.against >= rootDomainCount / 2) {
+            proposal.resolved = true;
+            emit ProposalResolved(proposalId, false);
+        }
     }
 
-    function getValidEntries() public view returns (string[] memory) {
-        uint256 count = 0;
+    function addEntry(string memory entry, string memory comment) external {
+        require(bytes(entry).length > 0, "Entry cannot be empty");
+        require(bytes(comment).length > 0, "Comment cannot be empty");
         
-        // Count valid entries
-        for (uint256 i = 0; i < entryKeys.length; i++) {
-            string memory entry = entryKeys[i];
-            if (allowList[entry].isVerified) {
-                count++;
+        Chainlink.Request memory req = buildChainlinkRequest(
+            jobId,
+            address(this),
+            this.fulfillDnsVerification.selector
+        );
+        req.add("get", entry);
+        bytes32 requestId = sendChainlinkRequest(req, oraclePayment);
+        requestIdToEntry[requestId] = entry;
+        dnsVerificationRequests[requestId] = DnsVerificationRequest({
+            entry: entry,
+            sender: msg.sender
+        });
+        allowList[entry] = Entry({
+            entry: entry,
+            rootDomain: entry,
+            comment: comment,
+            isVerified: false
+        });
+        entryKeys.push(entry);
+        emit EntryAdded(requestId, entry, comment);
+    }
+
+    function fulfillDnsVerification(
+        bytes32 requestId,
+        uint256 statusCode,
+        bytes32 data
+    ) external recordChainlinkFulfillment(requestId) {
+        DnsVerificationRequest memory dnsRequest = dnsVerificationRequests[
+            requestId
+        ];
+        string memory entry = dnsRequest.entry;
+
+        if (statusCode == 200 && data.length > 0) {
+            allowList[entry].isVerified = true;
+            emit EntryVerificationSucceeded(entry);
+        } else {
+            delete allowList[entry];
+            for (uint256 i = 0; i < entryKeys.length; i++) {
+                if (
+                    keccak256(abi.encodePacked(entryKeys[i])) ==
+                    keccak256(abi.encodePacked(entry))
+                ) {
+                    entryKeys[i] = entryKeys[entryKeys.length - 1];
+                    entryKeys.pop();
+                    break;
+                }
             }
+            emit EntryVerificationFailed(entry, "DNS Verification Failed");
         }
-        
-        string[] memory validEntries = new string[](count);
-        uint256 index = 0;
-        
-        // Populate array with valid entries
-        for (uint256 i = 0; i < entryKeys.length; i++) {
-            string memory entry = entryKeys[i];
-            if (allowList[entry].isVerified) {
-                validEntries[index] = entry;
-                index++;
-            }
-        }
-        return validEntries;
+        delete dnsVerificationRequests[requestId];
+    }
+
+    function updateEntry(string memory entry, string memory newComment)
+        external
+        validRootDomain(allowList[entry].rootDomain)
+    {
+        require(bytes(newComment).length > 0, "Comment cannot be empty");
+        allowList[entry].comment = newComment;
+        emit EntryUpdated(entry, newComment);
     }
 }
